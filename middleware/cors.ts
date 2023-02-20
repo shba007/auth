77aeebc002706355f569214342ac9fcd71162115
Shirsendu Bairagi <shirsendu2001@gmail.com>
2023-02-20 21:49:58 +0530

    cors middleware fixed pkgs updated

--- middleware/cors.ts
+++ middleware/cors.ts
@@ -4,13 +4,13 @@ export default defineEventHandler((event) => {
   setResponseHeaders(event, {
     "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "Access-Control-Allow-Origin": config.corsURL,
-    'Access-Control-Allow-Credentials': 'true',
-    "Access-Control-Allow-Headers": config.corsURL,
-    "Access-Control-Expose-Headers": config.corsURL
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Expose-Headers": "*"
   })
-  if (getMethod(event) === 'OPTIONS') {
+  if (getMethod(event) === "OPTIONS") {
     event.node.res.statusCode = 204
     event.node.res.statusMessage = "No Content."
-    return 'OK'
+    return "OK"
   }
 })
